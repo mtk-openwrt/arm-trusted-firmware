frank-w/u-boot

commit 6ea8b646e063e6f848deb4ef30814e41ecfbb55c

mediatek: mt7622: add initial BL2/BL31 support
Add initial BL2/BL31 support for mt7622.

Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

plat/mediatek/mt7622/aarch64/plat_helpers.S
@@ -1,0 +1,105 @@
+/*
+ * Copyright (c) 2013-2019, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <arch.h>
+#include <asm_macros.S>
+#include <mt7622_def.h>
+
+	.globl	plat_secondary_cold_boot_setup
+	.globl	plat_report_exception
+	.globl	platform_is_primary_cpu
+	.globl  plat_my_core_pos
+	.globl	plat_crash_console_init
+	.globl	plat_crash_console_putc
+	.globl	plat_crash_console_flush
+	.globl	read_cpuectlr
+	.globl	write_cpuectlr
+
+	/* -----------------------------------------------------
+	 * void plat_secondary_cold_boot_setup (void);
+	 *
+	 * This function performs any platform specific actions
+	 * needed for a secondary cpu after a cold reset e.g
+	 * mark the cpu's presence, mechanism to place it in a
+	 * holding pen etc.
+	 * -----------------------------------------------------
+	 */
+func plat_secondary_cold_boot_setup
+	/* MT7622 Oak does not do cold boot for secondary CPU */
+cb_panic:
+	b	cb_panic
+endfunc plat_secondary_cold_boot_setup
+
+func read_cpuectlr
+	MRS	x0, S3_1_C15_C2_1
+	ret
+endfunc	read_cpuectlr
+
+func write_cpuectlr
+	MSR	S3_1_C15_C2_1, x0
+	ret
+endfunc	write_cpuectlr
+
+func platform_is_primary_cpu
+	and	x0, x0, #(MPIDR_CLUSTER_MASK | MPIDR_CPU_MASK)
+	cmp	x0, #MT7622_PRIMARY_CPU
+	cset	x0, eq
+	ret
+endfunc platform_is_primary_cpu
+
+	/* -----------------------------------------------------
+	 * unsigned int plat_my_core_pos(void);
+	 *
+	 * result: CorePos = CoreId + (ClusterId << 2)
+	 * -----------------------------------------------------
+	 */
+func plat_my_core_pos
+	mrs     x0, mpidr_el1
+	and     x1, x0, #MPIDR_CPU_MASK
+	and     x0, x0, #MPIDR_CLUSTER_MASK
+	add     x0, x1, x0, LSR #6
+	ret
+endfunc plat_my_core_pos
+
+	/* ---------------------------------------------
+	 * int plat_crash_console_init(void)
+	 * Function to initialize the crash console
+	 * without a C Runtime to print crash report.
+	 * Clobber list : x0 - x4
+	 * ---------------------------------------------
+	 */
+func plat_crash_console_init
+	mov_imm	x0, UART_BASE
+	mov_imm	x1, UART_CLOCK
+	mov_imm	x2, UART_BAUDRATE
+	mov_imm	x3, 1
+	b	console_hsuart_init
+endfunc plat_crash_console_init
+
+	/* ---------------------------------------------
+	 * int plat_crash_console_putc(void)
+	 * Function to print a character on the crash
+	 * console without a C Runtime.
+	 * Clobber list : x1, x2
+	 * ---------------------------------------------
+	 */
+func plat_crash_console_putc
+	mov_imm x1, UART_BASE
+	b	console_hsuart_core_putc
+endfunc plat_crash_console_putc
+
+	/* ---------------------------------------------
+	 * int plat_crash_console_flush(int c)
+	 * Function to force a write of all buffered
+	 * data that hasn't been output.
+	 * Out : return -1 on error else return 0.
+	 * Clobber list : x0, x1
+	 * ---------------------------------------------
+	 */
+func plat_crash_console_flush
+	mov_imm	x0, UART_BASE
+	b	console_hsuart_core_flush
+endfunc plat_crash_console_flush

plat/mediatek/mt7622/bl2/bl2_plat_init.c
@@ -1,0 +1,52 @@
+/*
+ * Copyright (c) 2023, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <common/debug.h>
+#include <lib/mmio.h>
+#include <drivers/generic_delay_timer.h>
+#include <bl2_plat_setup.h>
+#include <platform_def.h>
+#include <mtk_wdt.h>
+#include <pinctrl.h>
+#include <pll.h>
+#include <pmic.h>
+#include <pmic_wrap_init.h>
+#include <cpuxgpt.h>
+
+static void mtk_print_cpu(void)
+{
+	NOTICE("CPU: MT%x\n", SOC_CHIP_ID);
+}
+
+static void mtk_wdt_init(void)
+{
+	mtk_wdt_control(false);
+}
+
+void bl2_el3_plat_arch_setup(void)
+{
+}
+
+bool plat_is_my_cpu_primary(void)
+{
+	return true;
+}
+
+const struct initcall bl2_initcalls[] = {
+	INITCALL(plat_mt_cpuxgpt_init),
+	INITCALL(generic_delay_timer_init),
+	INITCALL(mtk_wdt_print_status),
+	INITCALL(mtk_print_cpu),
+	INITCALL(mtk_pin_init),
+#ifndef IMAGE_BL2PL
+	INITCALL(mtk_pll_init),
+#endif
+	INITCALL(mtk_pwrap_init),
+	INITCALL(mtk_pmic_init),
+	INITCALL(mtk_wdt_init),
+
+	INITCALL(NULL)
+};

plat/mediatek/mt7622/aarch64/platform_common.c
@@ -1,0 +1,85 @@
+/*
+ * Copyright (c) 2013-2019, ARM Limited and Contributors. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <arch_helpers.h>
+#include <common/bl_common.h>
+#include <common/debug.h>
+#include <drivers/arm/cci.h>
+#include <lib/mmio.h>
+#include <lib/xlat_tables/xlat_tables_v2.h>
+#include <platform_def.h>
+
+static const int cci_map[] = {
+	CCI_CLUSTER0_SL_IFACE_IX,
+	CCI_CLUSTER1_SL_IFACE_IX
+};
+
+/* Table of regions to map using the MMU.  */
+const mmap_region_t plat_mmap[] = {
+	/* for TF text, RO, RW */
+	MAP_REGION_FLAT(TZRAM_BASE, TZRAM_SIZE,
+			MT_MEMORY | MT_RW | MT_SECURE),
+	MAP_REGION_FLAT(MTK_DEV_BASE, MTK_DEV_SIZE,
+			MT_DEVICE | MT_RW | MT_SECURE),
+	{ 0 }
+};
+
+/*******************************************************************************
+ * Macro generating the code for the function setting up the pagetables as per
+ * the platform memory map & initialize the mmu, for the given exception level
+ ******************************************************************************/
+#define DEFINE_CONFIGURE_MMU_EL(_el)					\
+	void plat_configure_mmu_el ## _el(unsigned long total_base,	\
+					  unsigned long total_size,	\
+					  unsigned long ro_start,	\
+					  unsigned long ro_limit,	\
+					  unsigned long coh_start,	\
+					  unsigned long coh_limit)	\
+	{								\
+		mmap_add_region(total_base, total_base,			\
+				total_size,				\
+				MT_MEMORY | MT_RW | MT_SECURE);		\
+		mmap_add_region(ro_start, ro_start,			\
+				ro_limit - ro_start,			\
+				MT_MEMORY | MT_RO | MT_SECURE);		\
+		mmap_add_region(coh_start, coh_start,			\
+				coh_limit - coh_start,			\
+				MT_DEVICE | MT_RW | MT_SECURE);		\
+		mmap_add(plat_mmap);					\
+		init_xlat_tables();					\
+									\
+		enable_mmu_el ## _el(0);				\
+	}
+
+/* Define EL3 variants of the function initialising the MMU */
+DEFINE_CONFIGURE_MMU_EL(3)
+
+unsigned int plat_get_syscnt_freq2(void)
+{
+	return read_cntfrq_el0();
+}
+
+void plat_cci_init(void)
+{
+	/* Initialize CCI driver */
+	cci_init(CCI_BASE, cci_map, ARRAY_SIZE(cci_map));
+	/* Eable non-secure access*/
+	mmio_write_32(CCI_BASE + CCI_SEC_ACCESS , 0x1);
+}
+
+void plat_cci_enable(void)
+{
+	/*
+	 * Enable CCI coherency for this cluster.
+	 * No need for locks as no other cpu is active at the moment.
+	 */
+	cci_enable_snoop_dvm_reqs(MPIDR_AFFLVL1_VAL(read_mpidr()));
+}
+
+void plat_cci_disable(void)
+{
+	cci_disable_snoop_dvm_reqs(MPIDR_AFFLVL1_VAL(read_mpidr()));
+}

plat/mediatek/mt7622/bl2/bl2.mk
@@ -1,0 +1,95 @@
+#
+# Copyright (c) 2023, MediaTek Inc. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+include lib/xz/xz.mk
+
+# BL2 image options
+BL2_IS_AARCH64		:=	1
+BL2_SIG_TYPE		:=	sha256+rsa-m1pss
+BL2_IMG_HDR_SOC		:=	mt7622
+
+BL2_CPPFLAGS		+=	-I$(MTK_PLAT_SOC)/drivers/pinctrl
+
+BL2_SOURCES		+=	common/desc_image_load.c			\
+				common/image_decompress.c			\
+				drivers/delay_timer/delay_timer.c		\
+				drivers/delay_timer/generic_delay_timer.c	\
+				drivers/gpio/gpio.c				\
+				drivers/io/io_storage.c				\
+				drivers/io/io_block.c				\
+				drivers/io/io_fip.c				\
+				lib/cpus/aarch64/cortex_a53.S			\
+				$(XZ_SOURCES)					\
+				$(APSOC_COMMON)/drivers/uart/aarch64/hsuart.S	\
+				$(MTK_PLAT_SOC)/aarch64/plat_helpers.S		\
+				$(MTK_PLAT_SOC)/aarch64/platform_common.c	\
+				$(MTK_PLAT_SOC)/bl2/bl2_plat_init.c		\
+				$(MTK_PLAT_SOC)/drivers/pinctrl/pinctrl.c	\
+				$(MTK_PLAT_SOC)/drivers/pll/pll.c		\
+				$(MTK_PLAT_SOC)/drivers/pmic/pmic_wrap.c	\
+				$(MTK_PLAT_SOC)/drivers/pmic/pmic.c		\
+				$(MTK_PLAT_SOC)/drivers/spm/mtcmos.c		\
+				$(MTK_PLAT_SOC)/drivers/timer/cpuxgpt.c		\
+				$(MTK_PLAT_SOC)/drivers/wdt/mtk_wdt.c
+
+# Trusted board boot
+include $(APSOC_COMMON)/bl2/tbbr.mk
+
+ifeq ($(TRUSTED_BOARD_BOOT),1)
+BL2_SOURCES		+=	plat/common/tbbr/plat_tbbr.c
+endif
+
+ifeq ($(BL2_COMPRESS),1)
+BL2_CPPFLAGS		+=	-DUSING_BL2PL
+endif # END OF BL2_COMPRESS
+
+BL2_BASE		:=	0x201000
+BL2_CPPFLAGS		+=	-DBL2_BASE=$(BL2_BASE)
+
+#
+# Boot device related build macros
+#
+include $(APSOC_COMMON)/bl2/bl2_image.mk
+
+ifndef BOOT_DEVICE
+$(echo You must specify the boot device by provide BOOT_DEVICE= to \
+	make parameter. Avaliable values: nor emmc sdmmc snand)
+else # NO BOOT_DEVICE
+
+ifeq ($(BOOT_DEVICE),ram)
+$(eval $(call BL2_BOOT_RAM))
+endif # END OF BOOT_DEVICE = ram
+
+ifeq ($(BOOT_DEVICE),nor)
+$(eval $(call BL2_BOOT_NOR_MMAP))
+BL2_SOURCES		+=	$(MTK_PLAT_SOC)/bl2/bl2_dev_spi_nor.c
+endif # END OF BOOTDEVICE = nor
+
+ifeq ($(BOOT_DEVICE),emmc)
+$(eval $(call BL2_BOOT_EMMC))
+BL2_SOURCES		+=	$(MTK_PLAT_SOC)/bl2/bl2_dev_mmc.c
+BL2_CPPFLAGS		+=	-DMSDC_INDEX=0
+endif # END OF BOOTDEVICE = emmc
+
+ifeq ($(BOOT_DEVICE),sdmmc)
+$(eval $(call BL2_BOOT_SD))
+BL2_SOURCES		+=	$(MTK_PLAT_SOC)/bl2/bl2_dev_mmc.c
+CPPFLAGS		+=	-DMSDC_INDEX=1
+DEVICE_HEADER_OFFSET	?=	0x80000
+endif # END OF BOOTDEVICE = sdmmc
+
+ifeq ($(BOOT_DEVICE),snand)
+$(eval $(call BL2_BOOT_SNFI,0,0))
+BL2_SOURCES		+=	$(MTK_PLAT_SOC)/bl2/bl2_dev_snfi.c
+NAND_TYPE		?=	2k+64
+$(eval $(call BL2_BOOT_NAND_TYPE_CHECK,$(NAND_TYPE),2k+64 2k+120 2k+128 4k+256))
+endif # END OF BOOTDEVICE = snand (snfi)
+
+ifeq ($(BROM_HEADER_TYPE),)
+$(error BOOT_DEVICE has invalid value. Please re-check.)
+endif
+
+endif # END OF BOOT_DEVICE

plat/mediatek/mt7622/bl2/bl2_dev_mmc.c
@@ -1,0 +1,94 @@
+/*
+ * Copyright (c) 2023, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <assert.h>
+#include <drivers/mmc.h>
+#include <drivers/mmc/mtk-sd.h>
+#include <lib/mmio.h>
+#include <platform_def.h>
+#include <pinctrl.h>
+
+struct msdc_gpio_mux_info {
+	const uint32_t *pins;
+	uint32_t count;
+	uint32_t mux;
+};
+
+static const struct msdc_compatible mt7622_msdc0_compat = {
+	.clk_div_bits = 12,
+	.pad_tune0 = true,
+	.async_fifo = true,
+	.data_tune = true,
+	.busy_check = true,
+	.stop_clk_fix = true,
+};
+
+static const struct msdc_compatible mt7622_msdc1_compat = {
+	.clk_div_bits = 12,
+	.pad_tune0 = true,
+	.async_fifo = true,
+	.data_tune = true,
+	.busy_check = true,
+	.stop_clk_fix = true,
+	.r_smpl = true,
+};
+
+static const uint32_t msdc0_pins[] = { 40, 41, 42, 43, 44, 45, 47, 48, 49, 50 };
+static const uint32_t msdc1_pins[] = { 16, 17, 18, 19, 20, 21 };
+
+static const struct msdc_gpio_mux_info msdc0_pinmux = {
+	.pins = msdc0_pins,
+	.count = ARRAY_SIZE(msdc0_pins),
+	.mux = 2,
+};
+
+static const struct msdc_gpio_mux_info msdc1_pinmux = {
+	.pins = msdc1_pins,
+	.count = ARRAY_SIZE(msdc1_pins),
+	.mux = 2,
+};
+
+static const struct mt7622_msdc_conf {
+	uintptr_t base;
+	uint32_t bus_width;
+	enum mmc_device_type type;
+	uint32_t src_clk;
+	const struct msdc_compatible *dev_comp;
+	const struct msdc_gpio_mux_info *pinmux;
+} mt7622_msdc[] = {
+	{
+		.base = MSDC0_BASE,
+		.bus_width = MMC_BUS_WIDTH_8,
+		.type = MMC_IS_EMMC,
+		.src_clk = 48000000,
+		.dev_comp = &mt7622_msdc0_compat,
+		.pinmux = &msdc0_pinmux,
+	}, {
+		.base = MSDC1_BASE,
+		.bus_width = MMC_BUS_WIDTH_4,
+		.type = MMC_IS_SD,
+		.src_clk = 48000000,
+		.dev_comp = &mt7622_msdc1_compat,
+		.pinmux = &msdc1_pinmux,
+	}
+};
+
+int mtk_plat_mmc_setup(uint32_t *num_sectors)
+{
+	const struct mt7622_msdc_conf *conf = &mt7622_msdc[MSDC_INDEX];
+	uint32_t i;
+
+	for (i = 0; i < conf->pinmux->count; i++)
+		mtk_set_pin_mode(conf->pinmux->pins[i], conf->pinmux->mux);
+
+	mtk_mmc_init(conf->base, 0, conf->dev_comp, conf->src_clk, conf->type,
+		     conf->bus_width);
+
+	if (num_sectors)
+		*num_sectors = mtk_mmc_block_count();
+
+	return 0;
+}

plat/mediatek/mt7622/bl2/bl2_dev_snfi.c
@@ -1,0 +1,52 @@
+/*
+ * Copyright (c) 2023, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <platform_def.h>
+#include <pinctrl.h>
+#include <mtk-snand.h>
+
+#define FIP_BASE			0x80000
+#define FIP_SIZE			0x200000
+
+struct snfi_gpio_mux_info {
+	const uint32_t *pins;
+	uint32_t count;
+	uint32_t mux;
+};
+
+static const uint32_t snfi_pins[] = { 8, 9, 10, 11, 12, 13 };
+
+static const struct snfi_gpio_mux_info snfi_pinmux = {
+	.pins = snfi_pins,
+	.count = ARRAY_SIZE(snfi_pins),
+	.mux = 2,
+};
+
+static const struct mtk_snand_platdata mt7622_snand_pdata = {
+	.nfi_base = (void *)NFI_BASE,
+	.ecc_base = (void *)NFIECC_BASE,
+	.soc = SNAND_SOC_MT7622,
+	.quad_spi = true
+};
+
+const struct mtk_snand_platdata *mtk_plat_get_snfi_platdata(void)
+{
+	uint32_t i;
+
+	for (i = 0; i < snfi_pinmux.count; i++)
+		mtk_set_pin_mode(snfi_pinmux.pins[i], snfi_pinmux.mux);
+
+	return &mt7622_snand_pdata;
+}
+
+void mtk_plat_fip_location(size_t *fip_off, size_t *fip_size)
+{
+	*fip_off = FIP_BASE;
+	*fip_size = FIP_SIZE;
+}

plat/mediatek/mt7622/bl2/bl2_dev_spi_nor.c
@@ -1,0 +1,17 @@
+/*
+ * Copyright (c) 2023, MediaTek Inc. All rights reserved.
+ *
+ * SPDX-License-Identifier: BSD-3-Clause
+ */
+
+#include <stddef.h>
+#include <stdint.h>
+
+#define FIP_BASE			0x20000
+#define FIP_SIZE			0x80000
+
+void mtk_plat_fip_location(size_t *fip_off, size_t *fip_size)
+{
+	*fip_off = FIP_BASE;
+	*fip_size = FIP_SIZE;
+}

plat/mediatek/mt7622/bl2pl/bl2pl.mk
@@ -1,0 +1,17 @@
+#
+# Copyright (c) 2023, MediaTek Inc. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+# BL2PL for BL2 compression
+ifeq ($(BL2_COMPRESS),1)
+BL2PL_SOURCES		+=	$(APSOC_COMMON)/bl2pl/udelay.c			\
+				$(MTK_PLAT_SOC)/bl2pl/bl2pl_plat_setup.c	\
+				$(MTK_PLAT_SOC)/bl2pl/serial.c			\
+				$(MTK_PLAT_SOC)/drivers/pll/pll.c		\
+				$(MTK_PLAT_SOC)/drivers/spm/mtcmos.c		\
+				$(MTK_PLAT_SOC)/drivers/timer/cpuxgpt.c
+
+BL2PL_CPPFLAGS		+=	-DXZ_SIMPLE_PRINT_ERROR
+endif # END OF BL2_COMPRESS

mtk-openwrt/arm-trusted-firmware

commit 9f5f90d7b8d3c3801d312bc702eeb7452d872446

mediatek: apsoc: move `plat_is_my_cpu_primary` into assembly helpers
This function is called before the C stack is set up, which resulted
in hangs when TF‐A is compiled with `-fno-omit-frame-pointer`,
as is now the default on several major Linux distributions.

As a slightly broken non‐stub implementation was already available
for the equivalent obsolete `platform_is_primary_cpu` API, fix and
rename it and drop the C functions.

If this logic is truly unnecessary, then `COLD_BOOT_SINGLE_CPU`
could be set instead and these functions removed.

Fixes: 6ea8b646e063e6f848deb4ef30814e41ecfbb55c ("mediatek: mt7622: add initial BL2/BL31 support")
Fixes: 36bebd65bf9f2af3504afbc6f8cd2c2e650be66b ("mediatek: mt7986: add initial BL2/BL31 support")
Fixes: fd8aecae81c0a34200bdaf3e75d7bdc3cf39070a ("mediatek: mt7981: add initial BL2/BL31 support")
Fixes: ced5322fb549e978057b09ef0ef529a893df74bc ("mediatek: mt7988: add initial BL2/BL31 support")
Fixes: e838b234347fa1149913c5cacf04ba8ca15cc2fa ("mediatek: mt7987: add initial BL2/BL31 support")
Signed-off-by: Emily <hello@emily.moe>

plat/mediatek/mt7622/aarch64/plat_helpers.S
@@ -11,7 +11,7 @@
 	.globl	platform_mem_init
 	.globl	plat_secondary_cold_boot_setup
 	.globl	plat_report_exception
-	.globl	platform_is_primary_cpu
+	.globl	plat_is_my_cpu_primary
 	.globl  plat_my_core_pos
 	.globl	plat_crash_console_init
 	.globl	plat_crash_console_putc
@@ -55,12 +55,13 @@ func write_cpuectlr
 	ret
 endfunc	write_cpuectlr
 
-func platform_is_primary_cpu
+func plat_is_my_cpu_primary
+	mrs	x0, mpidr_el1
 	and	x0, x0, #(MPIDR_CLUSTER_MASK | MPIDR_CPU_MASK)
 	cmp	x0, #MT7622_PRIMARY_CPU
 	cset	x0, eq
 	ret
-endfunc platform_is_primary_cpu
+endfunc plat_is_my_cpu_primary
 
 	/* -----------------------------------------------------
 	 * unsigned int plat_my_core_pos(void);

plat/mediatek/mt7622/bl2/bl2_plat_init.c
@@ -31,11 +31,6 @@ void bl2_el3_plat_arch_setup(void)
 {
 }
 
-bool plat_is_my_cpu_primary(void)
-{
-	return true;
-}
-
 const struct initcall bl2_initcalls[] = {
 	INITCALL(plat_mt_cpuxgpt_init),
 	INITCALL(generic_delay_timer_init),

plat/mediatek/mt7981/aarch64/plat_helpers.S
@@ -11,7 +11,7 @@
 	.globl	platform_mem_init
 	.globl	plat_secondary_cold_boot_setup
 	.globl	plat_report_exception
-	.globl	platform_is_primary_cpu
+	.globl	plat_is_my_cpu_primary
 	.globl  plat_my_core_pos
 	.globl	plat_crash_console_init
 	.globl	plat_crash_console_putc
@@ -55,12 +55,13 @@ func write_cpuectlr
 	ret
 endfunc	write_cpuectlr
 
-func platform_is_primary_cpu
+func plat_is_my_cpu_primary
+	mrs     x0, mpidr_el1
 	and	x0, x0, #(MPIDR_CLUSTER_MASK | MPIDR_CPU_MASK)
 	cmp	x0, #PRIMARY_CPU
 	cset	x0, eq
 	ret
-endfunc platform_is_primary_cpu
+endfunc plat_is_my_cpu_primary
 
 	/* -----------------------------------------------------
 	 * unsigned int plat_my_core_pos(void);

plat/mediatek/mt7981/bl2/bl2_plat_init.c
@@ -59,11 +59,6 @@ void bl2_el3_plat_arch_setup(void)
 {
 }
 
-bool plat_is_my_cpu_primary(void)
-{
-	return true;
-}
-
 const struct initcall bl2_initcalls[] = {
 	INITCALL(mtk_timer_init),
 	INITCALL(generic_delay_timer_init),

plat/mediatek/mt7986/aarch64/plat_helpers.S
@@ -11,7 +11,7 @@
 	.globl	platform_mem_init
 	.globl	plat_secondary_cold_boot_setup
 	.globl	plat_report_exception
-	.globl	platform_is_primary_cpu
+	.globl	plat_is_my_cpu_primary
 	.globl  plat_my_core_pos
 	.globl	plat_crash_console_init
 	.globl	plat_crash_console_putc
@@ -55,12 +55,13 @@ func write_cpuectlr
 	ret
 endfunc	write_cpuectlr
 
-func platform_is_primary_cpu
+func plat_is_my_cpu_primary
+	mrs     x0, mpidr_el1
 	and	x0, x0, #(MPIDR_CLUSTER_MASK | MPIDR_CPU_MASK)
 	cmp	x0, #PRIMARY_CPU
 	cset	x0, eq
 	ret
-endfunc platform_is_primary_cpu
+endfunc plat_is_my_cpu_primary
 
 	/* -----------------------------------------------------
 	 * unsigned int plat_my_core_pos(void);

plat/mediatek/mt7986/bl2/bl2_plat_init.c
@@ -83,11 +83,6 @@ void bl2_el3_plat_arch_setup(void)
 {
 }
 
-bool plat_is_my_cpu_primary(void)
-{
-	return true;
-}
-
 const struct initcall bl2_initcalls[] = {
 	INITCALL(mtk_timer_init),
 	INITCALL(generic_delay_timer_init),

plat/mediatek/mt7987/aarch64/plat_helpers.S
@@ -11,7 +11,7 @@
 	.globl	platform_mem_init
 	.globl	plat_secondary_cold_boot_setup
 	.globl	plat_report_exception
-	.globl	platform_is_primary_cpu
+	.globl	plat_is_my_cpu_primary
 	.globl  plat_my_core_pos
 	.globl	plat_crash_console_init
 	.globl	plat_crash_console_putc
@@ -54,12 +54,13 @@ func write_cpuectlr
 	ret
 endfunc	write_cpuectlr
 
-func platform_is_primary_cpu
+func plat_is_my_cpu_primary
+	mrs     x0, mpidr_el1
 	and	x0, x0, #(MPIDR_CLUSTER_MASK | MPIDR_CPU_MASK)
 	cmp	x0, #PRIMARY_CPU
 	cset	x0, eq
 	ret
-endfunc platform_is_primary_cpu
+endfunc plat_is_my_cpu_primary
 
 	/* -----------------------------------------------------
 	 * unsigned int plat_my_core_pos(void);

plat/mediatek/mt7987/bl2/bl2_plat_init.c
@@ -188,11 +188,6 @@ void bl2_el3_plat_arch_setup(void)
 	generic_delay_timer_init();
 }
 
-bool plat_is_my_cpu_primary(void)
-{
-	return true;
-}
-
 const struct initcall bl2_initcalls[] = {
 	INITCALL(mtk_wdt_init),
 	INITCALL(mtk_disable_PGD),

plat/mediatek/mt7988/aarch64/plat_helpers.S
@@ -11,7 +11,7 @@
 	.globl	platform_mem_init
 	.globl	plat_secondary_cold_boot_setup
 	.globl	plat_report_exception
-	.globl	platform_is_primary_cpu
+	.globl	plat_is_my_cpu_primary
 	.globl  plat_my_core_pos
 	.globl	plat_crash_console_init
 	.globl	plat_crash_console_putc
@@ -54,12 +54,13 @@ func write_cpuectlr
 	ret
 endfunc	write_cpuectlr
 
-func platform_is_primary_cpu
+func plat_is_my_cpu_primary
+	mrs     x0, mpidr_el1
 	and	x0, x0, #(MPIDR_CLUSTER_MASK | MPIDR_CPU_MASK)
 	cmp	x0, #PRIMARY_CPU
 	cset	x0, eq
 	ret
-endfunc platform_is_primary_cpu
+endfunc plat_is_my_cpu_primary
 
 	/* -----------------------------------------------------
 	 * unsigned int plat_my_core_pos(void);

plat/mediatek/mt7988/bl2/bl2_plat_init.c
@@ -174,11 +174,6 @@ void bl2_el3_plat_arch_setup(void)
 	generic_delay_timer_init();
 }
 
-bool plat_is_my_cpu_primary(void)
-{
-	return true;
-}
-
 const struct initcall bl2_initcalls[] = {
 	INITCALL(mtk_wdt_init),
 	INITCALL(mtk_disable_PGD),