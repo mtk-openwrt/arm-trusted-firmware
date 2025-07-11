frank-w/u-boot

mediatek: add common files for APSoC platform
The APSoC platform includes mt7622, mt7629, filogic chips and future chips.

These common files are shared between all chips list above.

Signed-off-by: Weijie Gao <weijie.gao@mediatek.com>

plat/mediatek/apsoc_common/bl2/bl2_image_post.mk
@@ -1,0 +1,86 @@
+#
+# Copyright (c) 2023, MediaTek Inc. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+#
+# Bromimage related build macros
+#
+DOIMAGEPATH		:=	tools/mediatek/bromimage
+DOIMAGETOOL		:=	$(DOIMAGEPATH)/bromimage
+
+# BL2 compress
+ifeq ($(BL2_COMPRESS),1)
+BL2PLIMAGEPATH		:= tools/bl2plimage
+BL2PLIMAGETOOL		:= $(BL2PLIMAGEPATH)/bl2plimage
+
+$(BL2PLIMAGETOOL):
+	$(Q)$(MAKE) --no-print-directory -C $(BL2PLIMAGEPATH)
+
+$(BUILD_PLAT)/bl2-sfx.bin: $(BUILD_PLAT)/bl2pl.bin $(BUILD_PLAT)/bl2.bin.xz.plimg
+	$(Q)cat $^ > $@
+
+$(BUILD_PLAT)/bl2.bin.xz.plimg: $(BUILD_PLAT)/bl2.bin.xz $(BL2PLIMAGETOOL)
+	$(Q)$(BL2PLIMAGETOOL) -a $(BL2_BASE) $(BUILD_PLAT)/bl2.bin.xz $@
+
+$(BUILD_PLAT)/bl2.bin.xz: $(BUILD_PLAT)/bl2.bin
+	$(ECHO) "  XZ      $@"
+	$(Q)xz -e -k -9 -C crc32 $< --stdout > $@
+
+.PHONY: $(BUILD_PLAT)/bl2.xz.plimg
+
+BL2_IMG_PAYLOAD := $(BUILD_PLAT)/bl2-sfx.bin
+else
+BL2_IMG_PAYLOAD := $(BUILD_PLAT)/bl2.bin
+endif # END OF BL2_COMPRESS
+
+# FIP compress
+ifeq ($(FIP_COMPRESS),1)
+BL31_PRE_TOOL_FILTER	:= XZ
+BL32_PRE_TOOL_FILTER	:= XZ
+BL33_PRE_TOOL_FILTER	:= XZ
+endif
+
+# Build dtb before embedding to BL2
+$(BUILD_PLAT)/bl2/dtb.o: $(BUILD_PLAT)/fdts/$(DTS_NAME).dtb
+
+ifeq ($(BOOT_DEVICE),ram)
+bl2: $(BL2_IMG_PAYLOAD)
+else
+bl2: $(BUILD_PLAT)/bl2.img
+endif
+
+ifneq ($(USE_MKIMAGE),1)
+ifneq ($(BROM_SIGN_KEY),)
+$(BUILD_PLAT)/bl2.img: $(BROM_SIGN_KEY)
+endif
+
+$(BUILD_PLAT)/bl2.img: $(BL2_IMG_PAYLOAD) $(DOIMAGETOOL)
+	-$(Q)rm -f $@.signkeyhash
+	$(Q)$(DOIMAGETOOL) 							\
+		-c $(BL2_IMG_HDR_SOC) -f $(BROM_HEADER_TYPE) -a $(BL2_BASE) -d	\
+		$(if $(DEVICE_HEADER_OFFSET), -o $(DEVICE_HEADER_OFFSET))	\
+		$(if $(BROM_SIGN_KEY), -s $(BL2_SIG_TYPE) -k $(BROM_SIGN_KEY))	\
+		$(if $(BROM_SIGN_KEY), -p $@.signkeyhash)			\
+		$(if $(BL_AR_VER), -r $(BL_AR_VER))				\
+		$(if $(NAND_TYPE), -n $(NAND_TYPE))				\
+		$(if $(BL2_IS_AARCH64), -e)					\
+		$(BL2_IMG_PAYLOAD) $@
+else
+MKIMAGE ?= mkimage
+
+ifneq ($(BROM_SIGN_KEY)$(ANTI_ROLLBACK),)
+$(warning BL2 signing/anti-rollback is not supported using mkimage)
+endif
+
+$(BUILD_PLAT)/bl2.img: $(BL2_IMG_PAYLOAD)
+	$(Q)$(MKIMAGE) -T mtk_image -a $(BL2_BASE) -e $(BL2_BASE)		\
+		-n "media=$(BROM_HEADER_TYPE)$(if $(NAND_TYPE),;nandinfo=$(NAND_TYPE))$(if $(BL2_IS_AARCH64),;arm64=1)$(if $(DEVICE_HEADER_OFFSET),;hdroffset=$(DEVICE_HEADER_OFFSET))"	\
+		-d $(BL2_IMG_PAYLOAD) $@
+endif
+
+$(DOIMAGETOOL): FORCE
+	$(Q)$(MAKE) --no-print-directory -C $(DOIMAGEPATH)
+
+.PHONY: $(BUILD_PLAT)/bl2.img

mtk-openwrt/arm-trusted-firmware

plat/mediatek/apsoc_common/bl2/bl2_image_post.mk
@@ -1,0 +1,86 @@
+#
+# Copyright (c) 2023, MediaTek Inc. All rights reserved.
+#
+# SPDX-License-Identifier: BSD-3-Clause
+#
+
+#
+# Bromimage related build macros
+#
+DOIMAGEPATH		:=	tools/mediatek/bromimage
+DOIMAGETOOL		:=	$(DOIMAGEPATH)/bromimage
+
+# BL2 compress
+ifeq ($(BL2_COMPRESS),1)
+BL2PLIMAGEPATH		:= tools/bl2plimage
+BL2PLIMAGETOOL		:= $(BL2PLIMAGEPATH)/bl2plimage
+
+$(BL2PLIMAGETOOL):
+	$(q)$(MAKE) --no-print-directory -C $(BL2PLIMAGEPATH)
+
+$(BUILD_PLAT)/bl2-sfx.bin: $(BUILD_PLAT)/bl2pl.bin $(BUILD_PLAT)/bl2.bin.xz.plimg
+	$(q)cat $^ > $@
+
+$(BUILD_PLAT)/bl2.bin.xz.plimg: $(BUILD_PLAT)/bl2.bin.xz $(BL2PLIMAGETOOL)
+	$(q)$(BL2PLIMAGETOOL) -a $(BL2_BASE) $(BUILD_PLAT)/bl2.bin.xz $@
+
+$(BUILD_PLAT)/bl2.bin.xz: $(BUILD_PLAT)/bl2.bin
+	$(s)echo "  XZ      $@"
+	$(q)xz -e -k -9 -C crc32 $< --stdout > $@
+
+.PHONY: $(BUILD_PLAT)/bl2.xz.plimg
+
+BL2_IMG_PAYLOAD := $(BUILD_PLAT)/bl2-sfx.bin
+else
+BL2_IMG_PAYLOAD := $(BUILD_PLAT)/bl2.bin
+endif # END OF BL2_COMPRESS
+
+# FIP compress
+ifeq ($(FIP_COMPRESS),1)
+BL31_PRE_TOOL_FILTER	:= XZ
+BL32_PRE_TOOL_FILTER	:= XZ
+BL33_PRE_TOOL_FILTER	:= XZ
+endif
+
+# Build dtb before embedding to BL2
+$(BUILD_PLAT)/bl2/dtb.o: $(BUILD_PLAT)/fdts/$(DTS_NAME).dtb
+
+ifeq ($(BOOT_DEVICE),ram)
+bl2: $(BL2_IMG_PAYLOAD)
+else
+bl2: $(BUILD_PLAT)/bl2.img
+endif
+
+ifneq ($(USE_MKIMAGE),1)
+ifneq ($(BROM_SIGN_KEY),)
+$(BUILD_PLAT)/bl2.img: $(BROM_SIGN_KEY)
+endif
+
+$(BUILD_PLAT)/bl2.img: $(BL2_IMG_PAYLOAD) $(DOIMAGETOOL)
+	-$(q)rm -f $@.signkeyhash
+	$(q)$(DOIMAGETOOL) 							\
+		-c $(BL2_IMG_HDR_SOC) -f $(BROM_HEADER_TYPE) -a $(BL2_BASE) -d	\
+		$(if $(DEVICE_HEADER_OFFSET), -o $(DEVICE_HEADER_OFFSET))	\
+		$(if $(BROM_SIGN_KEY), -s $(BL2_SIG_TYPE) -k $(BROM_SIGN_KEY))	\
+		$(if $(BROM_SIGN_KEY), -p $@.signkeyhash)			\
+		$(if $(BL_AR_VER), -r $(BL_AR_VER))				\
+		$(if $(NAND_TYPE), -n $(NAND_TYPE))				\
+		$(if $(BL2_IS_AARCH64), -e)					\
+		$(BL2_IMG_PAYLOAD) $@
+else
+MKIMAGE ?= mkimage
+
+ifneq ($(BROM_SIGN_KEY)$(ANTI_ROLLBACK),)
+$(warning BL2 signing/anti-rollback is not supported using mkimage)
+endif
+
+$(BUILD_PLAT)/bl2.img: $(BL2_IMG_PAYLOAD)
+	$(q)$(MKIMAGE) -T mtk_image -a $(BL2_BASE) -e $(BL2_BASE)		\
+		-n "media=$(BROM_HEADER_TYPE)$(if $(NAND_TYPE),;nandinfo=$(NAND_TYPE))$(if $(BL2_IS_AARCH64),;arm64=1)$(if $(DEVICE_HEADER_OFFSET),;hdroffset=$(DEVICE_HEADER_OFFSET))"	\
+		-d $(BL2_IMG_PAYLOAD) $@
+endif
+
+$(DOIMAGETOOL): FORCE
+	$(q)$(MAKE) --no-print-directory -C $(DOIMAGEPATH)
+
+.PHONY: $(BUILD_PLAT)/bl2.img